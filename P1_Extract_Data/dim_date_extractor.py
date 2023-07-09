001081abd3fda4cb0956e03567d334305b9ff5f6
VinhTran <15521020@gm.uit.edu.vn>
2023-07-09 19:50:06 +0700

    upload dim date, dim time as zip

--- P1_Extract_Data/dim_date_extractor.py
+++ P1_Extract_Data/dim_date_extractor.py
@@ -7,8 +7,8 @@ secret_access_key =  os.environ['SECRET_ACCESS_KEY']
 s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 bucket_name = os.environ['S3_DATA_LAKEHOUSE_BUCKET_NAME'] 
 
-dim_dates_file_path = os.path.join(Path(__file__).parents[1], 'data', 'dimdate.csv')
-s3_path_to_upload = 'raw/dimension-date/dimension_date.csv'
+dim_dates_file_path = os.path.join(Path(__file__).parents[1], 'data', 'dimdate.csv.zip')
+s3_path_to_upload = 'raw/dimension-date/dimension_date.csv.zip'
 print(dim_dates_file_path)
 with open(dim_dates_file_path, "rb") as f:
     print('Uploading dimension date')
